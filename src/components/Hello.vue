9efb7c7c19562e2ab0d9321fa3498a317b4057bf
JunHyeongB <aowlr0513@gmail.com>
2021-09-29 17:44:15 +0900

    Create project

---
+++ src/components/Hello.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello 안녕안녕</h1>
+</template>
+
+<style scoped lang="scss">
+  $color: blue;
+  h1 {
+    color: $color;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: ['.vue', '.js'],
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
+  },
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          "sass-loader"
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' } // to는 생략해도 path 옵션을 통해 'dist'를 찾아감
+      ]
+    })
+  ],
+  // devServer: {
+  //   port: 1234, // localhost 포트 번호를 바꿀 수 있다.(기본 8080)
+  // },
+};
